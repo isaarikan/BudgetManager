fdbf4fa65d8bead604e86fe82c060cdd89cb2ea0
isaarikan <isaarikantr@gmail.com>
2017-04-18 19:41:53 +0300

    Initial commit

---
+++ app/src/main/java/Adapter/CustomAdapter.java
@@ -0,0 +1,66 @@
+package Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import Model.Income;
+import arksoft.com.budgetmanager.R;
+
+/**
+ * Created by isaarikan on 18.04.2017.
+ */
+
+public class CustomAdapter extends BaseAdapter{
+    Context c;
+    ArrayList<Income> incomes;
+
+    public CustomAdapter(Context c,ArrayList<Income> incomes){
+        this.incomes=incomes;
+        this.c=c;
+
+
+    }
+
+    @Override
+    public int getCount() {
+        return incomes.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return incomes.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if(convertView==null){
+            convertView= LayoutInflater.from(c).inflate(R.layout.model,parent,false);
+        }
+
+        TextView nameTxt= (TextView) convertView.findViewById(R.id.nameTxt);
+        TextView propTxt= (TextView) convertView.findViewById(R.id.propellantTxt);
+        TextView descTxt= (TextView) convertView.findViewById(R.id.descTxt);
+
+
+
+        final Income s= (Income) this.getItem(position);
+
+        nameTxt.setText(s.getName());
+        propTxt.setText(s.getType());
+        //descTxt.setText(s.getMiktar());
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/arksoft/com/budgetmanager/MainActivity.java
@@ -0,0 +1,107 @@
+package arksoft.com.budgetmanager;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import Adapter.CustomAdapter;
+import FireBase.FireBaseHelper;
+import Model.Income;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseReference db;
+    FireBaseHelper helper;
+    FloatingActionButton fab;
+    CustomAdapter adapter;
+    ListView lv;
+    EditText nameEditTxt, propTxt, descTxt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_content);
+
+        //Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+       // setSupportActionBar(toolbar);
+        lv=(ListView)findViewById(R.id.lv);
+
+        db= FirebaseDatabase.getInstance().getReference();
+        helper=new FireBaseHelper(db);
+        adapter=new CustomAdapter(this,helper.retrieve());
+        lv.setAdapter(adapter);
+
+        adapter = new CustomAdapter(MainActivity.this, helper.retrieve());
+        lv.setAdapter(adapter);
+        fab=(FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayInput();
+            }
+        });
+
+
+
+
+    }
+
+    private void displayInput() {
+        final Dialog d=new Dialog(this);
+
+        d.setTitle("Gelir Girin");
+        d.setContentView(R.layout.inputlayout);
+
+        nameEditTxt = (EditText) d.findViewById(R.id.nameEditText);
+        propTxt = (EditText) d.findViewById(R.id.propellantEditText);
+        descTxt = (EditText) d.findViewById(R.id.descEditText);
+        Button saveBtn = (Button) d.findViewById(R.id.saveBtn);
+
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEditTxt.getText().toString();
+                String type = propTxt.getText().toString();
+                int  amount= Integer.parseInt(descTxt.getText().toString());
+
+                Income income=new Income(name,type,amount);
+
+                income.setName(name);
+                income.setType(type);
+                income.setMiktar(amount);
+
+
+                if (name != null && name.length() > 0) {
+                    //THEN SAVE
+                    if (helper.save(income)) {
+                        //IF SAVED CLEAR EDITXT
+                        d.cancel();
+                        nameEditTxt.setText("");
+                        propTxt.setText("");
+                        descTxt.setText("");
+                        adapter = new CustomAdapter(MainActivity.this, helper.retrieve());
+                        lv.setAdapter(adapter);
+
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Name Must Not Be Empty", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        d.show();
+
+
+
+
+    }
+}
